8c62d975f3cc00574ecdc73ba73c7f196f108be2
NikitosikQQ <akmn00@mail.ru>
2022-12-21 18:36:12 +0300

    Откорректированный вариант 1

--- src/CreditCalc.java
+++ src/CreditCalc.java
@@ -2,40 +2,37 @@ import java.util.Scanner;
 
 public class CreditCalc {
 
-    int Years;
+    int years;
 
-//    public void setCredit(double credit){
-//
-//    }
-//
-//    public void setPayment(double payment) {
-//
-//    }
-//
-//    public void setPercent(double percent) {
-//
-//    }
-//
-//    public void setClient(String client) {
-//        this.client = client;
-//    }
+    public static int getVerification(double credit, double payment, double percent, String client) {
+        if (credit > 0 && payment > 0 && percent > 0 && credit > payment && payment > percent) {
+            if (client.equalsIgnoreCase("business")) {
+                boolean possibility = ((credit - (payment * 12)) + (credit / 100 * percent) - (payment * 12) < credit);
+                if (possibility) {
+                    return 1;
+                } else return 0;
+            } else if (credit + (credit / 100 * percent) - (payment * 12) < credit) {
+                return 1;
+            } else return 0;
+        } else return 0;
+    }
 
     public void getOverpayment(double credit, double payment, double percent, String client) {
 
-        Years = (int)Math.ceil((credit*(percent/100+1))/payment/12);
-        //System.out.println("количество циклов годов = "+ Years);
+        years = (int) Math.ceil((credit * (percent / 100 + 1)) / payment / 12);
         double sum = 0;
-        double ostatok;
+        double balance;
 
-        for (int i=0; i<Years; i++){
-            if((client.equalsIgnoreCase("business")) && (i == 0)){
-                ostatok = credit;
+        for (int i = 0; i < years; i++) {
+            if ((client.equalsIgnoreCase("business")) && (i == 0)) {
+                balance = credit;
                 sum = 0;
-            }else {
-                ostatok = credit+(credit/100*percent);
-                    sum = sum + credit*(percent/100);}
-            credit = ostatok - (payment*12);
+            } else {
+                balance = credit + (credit / 100 * percent);
+                sum = sum + credit * (percent / 100);
+            }
+            credit = balance - (payment * 12);
         }
-System.out.println("Сумма переплаты по кредиту = "+ sum);
+        System.out.println("Сумма переплаты по кредиту = " + sum);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,38 @@
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите данные по кредиту в следующем порядке:Сумма денег в кредите," +
-                " ежемес.платеж, процентная ставка, тип клиента через пробел");
-        String scString = sc.nextLine();
-        String[] scStrings = scString.split(" ");
-        double[] scNumbers = new double[scStrings.length];
-        String Hu = "human";
-        String Bu = "business";
-        boolean checkHu = Hu.equalsIgnoreCase(scStrings[3]);
-        boolean checkBu = Bu.equalsIgnoreCase(scStrings[3]);
-//        System.out.println(checkBu);
-//        System.out.println(checkHu);
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите данные по кредиту через пробел:");
+        String scString = sc.nextLine();
+        String[] creditInfo = scString.split(" ");
+        double[] creditValues = new double[creditInfo.length];
+
+        String Hum = "human";
+        String Bus = "business";
+
+//
         CreditCalc calc = new CreditCalc();
 
-        if (scStrings.length == 4) {
-            if(checkHu==true || checkBu==true) {
-                try {
-                    for (int i = 0; i < scStrings.length - 1; i++) {
-                        scNumbers[i] = Double.parseDouble(scStrings[i]);
+        if (creditInfo.length == 4) {
+            try {
+                boolean checkHum = Hum.equalsIgnoreCase(creditInfo[3]);
+                boolean checkBus = Bus.equalsIgnoreCase(creditInfo[3]);
+                if (checkHum == true || checkBus == true) {
+                    for (int i = 0; i < creditInfo.length - 1; i++) {
+                        creditValues[i] = Double.parseDouble(creditInfo[i]);
                     }
-                    if (Main.condition(scNumbers[0], scNumbers[1], scNumbers[2], scStrings[3]) == 1){
-                        calc.getOverpayment(scNumbers[0], scNumbers[1], scNumbers[2], scStrings[3]);
-                    }else System.out.println("Введены невозможные значения по кредиту!");
-                } catch (NumberFormatException | ArrayIndexOutOfBoundsException | NullPointerException t) {
-                    System.out.println("Значения введены некорректно!");
-                }
-            }else System.out.println("Некорректно введен тип клиента!");
+                    if (CreditCalc.getVerification(creditValues[0], creditValues[1], creditValues[2], creditInfo[3]) == 1) {
+                        calc.getOverpayment(creditValues[0], creditValues[1], creditValues[2], creditInfo[3]);
+                    } else System.out.println("Введены невозможные значения по кредиту!");
+                } else System.out.println("Некорректно введен тип клиента!");
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException | NullPointerException t) {
+                System.out.println("Значения введены некорректно!");
+            }
+
         } else System.out.println("Введено больше или меньше значений для расчета! ");
     }
-    public static int condition (double i0, double i1, double i2, String i3) {
-        if (i0 > 0 && i1 > 0 && i2 > 0 && i0 > i1 && i1 > i2) {
-            if (i3.equalsIgnoreCase("business")){
-             boolean C = ((i0-(i1*12))+(i0/100*i2)-(i1*12) < i0);
-             if (C) return 1;else return 0;
-            } else
-             if(i0+(i0/100*i2)-(i1*12) < i0) return 1; else return 0;
-
-        } else return 0;
-    }
-
 }
+
 
